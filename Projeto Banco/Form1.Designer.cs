4d6eafab04255b1038e9d31c4a901a80b01da7d1
Letícia Schmidt Baccelli <leticiaschmidtbaccelli@gmail.com>
2021-06-28 13:46:34 -0300

    Subindo projeto ds para o github

---
+++ Projeto Banco/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alessandro
+ * Date: 26/04/2021
+ * Time: 13:46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Projeto_Banco
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label5 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.txtemail = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txttel = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtnome = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtcodigo = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(84, 33);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(140, 23);
+			this.label5.TabIndex = 19;
+			this.label5.Text = "Projeto Banco";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(97, 255);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(96, 32);
+			this.button1.TabIndex = 18;
+			this.button1.Text = "Cadastrar";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// txtemail
+			// 
+			this.txtemail.Location = new System.Drawing.Point(84, 204);
+			this.txtemail.Name = "txtemail";
+			this.txtemail.Size = new System.Drawing.Size(188, 20);
+			this.txtemail.TabIndex = 17;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(12, 204);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(66, 23);
+			this.label4.TabIndex = 16;
+			this.label4.Text = "Email:";
+			// 
+			// txttel
+			// 
+			this.txttel.Location = new System.Drawing.Point(84, 164);
+			this.txttel.Name = "txttel";
+			this.txttel.Size = new System.Drawing.Size(188, 20);
+			this.txttel.TabIndex = 15;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(12, 164);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(77, 23);
+			this.label3.TabIndex = 14;
+			this.label3.Text = "Telefone:";
+			// 
+			// txtnome
+			// 
+			this.txtnome.Location = new System.Drawing.Point(84, 122);
+			this.txtnome.Name = "txtnome";
+			this.txtnome.Size = new System.Drawing.Size(188, 20);
+			this.txtnome.TabIndex = 13;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(12, 122);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(66, 23);
+			this.label2.TabIndex = 12;
+			this.label2.Text = "Nome:";
+			// 
+			// txtcodigo
+			// 
+			this.txtcodigo.Location = new System.Drawing.Point(84, 86);
+			this.txtcodigo.Name = "txtcodigo";
+			this.txtcodigo.Size = new System.Drawing.Size(188, 20);
+			this.txtcodigo.TabIndex = 11;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 86);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(66, 23);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "Código:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 321);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.txtemail);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.txttel);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtnome);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtcodigo);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtcodigo;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtnome;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txttel;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox txtemail;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label5;
+	}
+}
---
+++ Projeto Banco/Form1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alessandro
+ * Date: 26/04/2021
+ * Time: 13:46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Projeto_Banco
+{
+	/// <summary>
+	/// Description of Form1.
+	/// </summary>
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
